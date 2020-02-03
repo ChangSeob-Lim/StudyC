64ff8d9f13843dbafdb97028ea2b708492701f39
ChangSeob-Lim <limchangsz@gmail.com>
2020-02-03 09:37:43 +0900

    템플릿 변경

---
+++ OpTestApp/main.c
@@ -0,0 +1,23 @@
+/*
+  filename - main.c
+  version - 1.0
+  description - 기본 메인 함수
+  --------------------------------------------------------------------------------
+  first created - 2020.02.01
+  writer - Hugo MG Sung.
+  last modify - 
+*/
+// 기본 양식 주석을 넣어주는게 일반적임 (헤더주석)
+// 본인 관리용
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 메인함수
+int main(void) 
+{
+	printf("Hello World!\n");
+    // type here.
+	system("pause");
+	return EXIT_SUCCESS;
+}
