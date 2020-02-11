09e2f026c9564a844292b9a4cc1e768aa3b76d91
ChangSeob-Lim <limchangsz@gmail.com>
2020-02-11 17:18:08 +0900

    2/11

---
+++ MyMathLib/mycalc.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+	_declspec(dllimport) int int_add(int a, int b);
+	_declspec(dllimport) int int_multi(int a, int b);
+#ifdef __cplusplus
+}
+#endif // __cplusplus
